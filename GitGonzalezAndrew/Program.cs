25affc71269ebc2b96c2de372a5008bf13262d79
andr3w56 <andrewgonzale009@gmail.com>
2022-11-11 13:31:50 -0500

    Initial Commit

---
+++ GitGonzalezAndrew/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGonzalezAndrew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string apellidos = "González Castillo";
+            const string nombres = "Andrew Fernando";
+            const string curso = "Segundo TICS";
+            const string asignatura = "Programación Estructurada";
+            const string fecha = "11/11/2022";
+            const string tema = "Operadores";
+
+            Console.WriteLine(apellidos);
+            Console.WriteLine(nombres);
+            Console.WriteLine(curso);
+            Console.WriteLine(asignatura);
+            Console.WriteLine(fecha);
+            Console.WriteLine(tema);
+        }
+    }
+}

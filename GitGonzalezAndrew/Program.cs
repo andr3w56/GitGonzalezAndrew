1b89005d5b6a4a8f1b482e1e04e55f8114e308d8
andr3w56 <andrewgonzale009@gmail.com>
2022-11-11 14:19:28 -0500

    Agregando dias con numeros aleatorios

--- GitGonzalezAndrew/Program.cs
+++ GitGonzalezAndrew/Program.cs
@@ -10,6 +10,15 @@ namespace GitGonzalezAndrew
     {
         static void Main(string[] args)
         {
+            
+            /*
+             * 
+             * Datos Informativos
+             * 
+             */
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Datos Informativos\n");
+
             const string apellidos = "González Castillo";
             const string nombres = "Andrew Fernando";
             const string curso = "Segundo TICS";
@@ -17,12 +26,39 @@ namespace GitGonzalezAndrew
             const string fecha = "11/11/2022";
             const string tema = "Operadores";
 
-            Console.WriteLine(apellidos);
-            Console.WriteLine(nombres);
-            Console.WriteLine(curso);
-            Console.WriteLine(asignatura);
-            Console.WriteLine(fecha);
-            Console.WriteLine(tema);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Apellidos: {apellidos}");
+            Console.WriteLine($"Nombres: {nombres}");
+            Console.WriteLine($"Curso: {curso}");
+            Console.WriteLine($"Asignatura: {asignatura}");
+            Console.WriteLine($"Fecha: {fecha}");
+            Console.WriteLine($"Tema: {tema}");
+            /*
+             * 
+             * Dias de la semana.
+             * 
+             */
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Número Aleatorio\n");
+
+            Random rnd = new Random();
+            int aleatorio = rnd.Next(1, 8);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\nEl número aleatorio es: {aleatorio}");
+
+            string[] diasSemanales = {"Lunes", "Martes", "Miércoles", "Jueves",
+                "Viernes", "Sábado", "Domingo"};
+
+            for (int i = 0; i <= 7; i++)
+            {
+                if (aleatorio == i)
+                {
+                    Console.WriteLine(diasSemanales[i - 1]);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
